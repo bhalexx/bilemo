c9a36e40114b09881237ef0459eba46848999457
Bhalexx <minia-ture@hotmail.com>
2017-12-07 12:23:55 +0100

    Created Application CRUD

---
+++ api/src/AppBundle/Controller/ApplicationController.php
@@ -0,0 +1,116 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use AppBundle\Entity\Application;
+
+class ApplicationController extends FOSRestController
+{
+    /**
+     * @Rest\Get(
+     *     path = "/applications",
+     *     name = "api_application_list"
+     * )
+     * @Rest\View(
+     *     statusCode = 200
+     * )
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $applications = $em->getRepository('AppBundle:Application')->findAll();
+
+        return $applications;
+    }
+
+    /**
+     * @Rest\Get(
+     *     path = "/applications/{id}",
+     *     name = "api_application_view",
+     *     requirements = { "id" = "\d+" }
+     * )
+     *
+     * @Rest\View(
+     *     statusCode = 200
+     * )
+     */
+    public function viewAction(Application $application)
+    {
+        return $application;
+    }
+
+    /**
+     * @Rest\Post(
+     *     path = "/applications",
+     *     name = "api_application_create"
+     * )
+     * @Rest\View(
+     *     statusCode = 201
+     * )
+     *
+     * @ParamConverter("application", converter="fos_rest.request_body")
+     */
+    public function createAction(Application $application)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($application);
+        $em->flush();
+
+        return $this->view(
+            $application,
+            Response::HTTP_CREATED,
+            ['Location' => $this->generateUrl('api_application_view', ['id' => $application->getId(), UrlGeneratorInterface::ABSOLUTE_URL])]
+        );
+    }
+
+    /**
+     * @Rest\Put(
+     *     path = "applications/{id}",
+     *     name = "api_application_update",
+     *     requirements = { "id" = "\d+" }
+     * )
+     * @Rest\View(
+     *     statusCode = 200
+     * )
+     *
+     * @ParamConverter("newApplication", converter="fos_rest.request_body")
+     */
+    public function updateAction(Application $application, Application $newApplication)
+    {
+        $application->setName($newApplication->getName());
+        $this->getDoctrine()->getManager()->flush();
+
+        return $application;
+    }
+
+    /**
+     * @Rest\Delete(
+     *     path = "applications/{id}",
+     *     name = "api_application_delete",
+     *     requirements = { "id" = "\d+" }
+     * )
+     * @Rest\View(
+     *     statusCode = 204
+     * )
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $application = $em->getRepository('AppBundle:Application')->findOneById($id);
+
+        if ($application) {
+            $em->remove($application);
+            $em->flush();
+        }
+
+        return;
+    }
+}
