7e6462e46ce92f7da957198ef3cae521b13c9ced
Bhalexx <minia-ture@hotmail.com>
2018-03-15 16:40:20 +0100

    Service for picture path modification for API clients

--- api/src/AppBundle/Controller/MobileController.php
+++ api/src/AppBundle/Controller/MobileController.php
@@ -74,10 +74,6 @@ class MobileController extends FOSRestController
      */
     public function viewAction(Mobile $mobile)
     {
-        //Edit mobile pictures path
-        foreach ($mobile->getPictures() as $picture) {
-            $picture->setPath($this->container->getParameter('picture_path').$picture->getPath());
-        }
         return $mobile;
     }
 
