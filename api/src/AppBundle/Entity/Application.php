b38431d8bd94bf7896598fa60c8b7e394715af5a
Bhalexx <minia-ture@hotmail.com>
2018-03-23 18:07:15 +0100

    Application users relation bug fix

--- api/src/AppBundle/Entity/Application.php
+++ api/src/AppBundle/Entity/Application.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Hateoas\Configuration\Annotation as Hateoas;
